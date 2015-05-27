2aa2ce60f315742f9602e36beebc5f776558aa14
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-05-28 07:37:11 +0900

    並び替え面倒なので一旦なしでw

--- dev/assets/scripts/app/stores/PropertyStore.js
+++ dev/assets/scripts/app/stores/PropertyStore.js
@@ -8,7 +8,7 @@ import Store from '../../framework/Store';
 class PropertyStore extends Store {
   constructor() {
     super();
-    this._properties = {};
+    this._properties = this.loadData() || {};
   }
 
   /**
@@ -18,13 +18,12 @@ class PropertyStore extends Store {
   */
   _create(name, amount) {
     let id = (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
-    let order = Object.keys(this._properties).length;
     this._properties[id] = {
       id: id,
       name: name,
-      amount: amount,
-      order: order
+      amount: amount
     };
+    this.saveData();
   }
 
   /**
@@ -34,6 +33,7 @@ class PropertyStore extends Store {
   */
   _update(id, updates) {
     this._properties[id] = Object.assign({}, this._properties[id], updates);
+    this.saveData();
   }
 
   /**
@@ -42,49 +42,14 @@ class PropertyStore extends Store {
   */
   _destroy(id) {
     delete this._properties[id];
+    this.saveData();
   }
 
-  /**
-    資産の表示順を変更するmethod
-    @param {number} from  - 移動する資産
-    @param {number} to - 移動先
-  */
- // FIXME: fromをidに変更した方がいい？
-  _updateOrders(from, to) {
-    for (let id in this._properties) {
-      if (!{}.hasOwnProperty.call(this._properties, id)) return false;
-      let property = this._properties[id];
-      if (from > to) {
-        if (property.order >= to && property.order < from) {
-          this._update(id, {order: this._properties[id].order + 1});
-        } else if (property.order === from) {
-          this._update(id, {order: to});
-        }
-      } else if (to > from) {
-        if (property.order > from && property.order <= to) {
-          this._update(id, {order: this._properties[id].order - 1});
-        } else if (property.order === from) {
-          this._update(id, {order: to});
-        }
-      }
-    }
-  }
   /**
     資産一覧を取得するmethod
   */
   getAll() {
-    let properties = [];
-    for (let key in this._properties) {
-      if (!{}.hasOwnProperty.call(this._todos, key)) return false;
-      let property = this._properties[key];
-      properties.push(property);
-    }
-    properties.sort((a, b) => {
-      let x = a.order;
-      let y = b.order;
-      return (x > y) ? 1 : -1;
-    });
-    return properties;
+    return this._properties;
   }
   /**
     資産を保存するmethod
