539aa2a4675e9149387714b7225a82844a904966
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-05-30 23:02:55 +0900

    ESDocとりあえず止め

--- dev/assets/scripts/app/stores/PropertyStore.js
+++ dev/assets/scripts/app/stores/PropertyStore.js
@@ -1,21 +1,11 @@
 import 'babel/polyfill';
 import Store from '../../framework/Store';
 
-/**
- * @classdesc 資産
- * @constructor
- */
 class PropertyStore extends Store {
   constructor() {
     super();
-    this._properties = this.loadData() || {};
+    this._properties = this._load() || {};
   }
-
-  /**
-    資産を追加するmethod
-    @param {string} name - 資産名
-    @param {number} amount - 初期資産
-  */
   _create(name, amount) {
     let id = (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
     this._properties[id] = {
@@ -23,46 +13,23 @@ class PropertyStore extends Store {
       name: name,
       amount: amount
     };
-    this.saveData();
+    this._save();
   }
-
-  /**
-    資産を更新するmethod
-    @param {string} id - 資産id
-    @param {object} updates - 資産Object
-  */
   _update(id, updates) {
     this._properties[id] = Object.assign({}, this._properties[id], updates);
-    this.saveData();
+    this._save();
   }
-
-  /**
-    資産を削除するmethod
-    @param {string} id - 資産id
-  */
   _destroy(id) {
     delete this._properties[id];
-    this.saveData();
+    this._save();
   }
-
-  /**
-    資産一覧を取得するmethod
-  */
   getAll() {
     return this._properties;
   }
-  /**
-    資産を保存するmethod
-    TODO: indexedDBに書き換えたいが、ちょっと知見足りないorz
-  */
-  saveData() {
+  _save() {
     localStorage.setItem('_properties', JSON.stringify(this._properties));
   }
-  /**
-    資産を読み込むするmethod
-    TODO: indexedDBに書き換えたいが、ちょっと知見足りないorz
-  */
-  loadData() {
+  _load() {
     return JSON.parse(localStorage.getItem('_properties'));
   }
 }
