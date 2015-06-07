8dca13fda4d4eb06ceb09bde1878c2f07aa7e83d
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-06-07 16:48:45 +0900

    編集方法を変更

--- dev/assets/scripts/app/components/PropertyItem.js
+++ dev/assets/scripts/app/components/PropertyItem.js
@@ -3,13 +3,18 @@ import PropertyActions from '../actions/PropertyActions';
 
 export default class PropertyItem extends Component {
   constructor(property) {
-    super('template', {}, {
+    super('template', {
+      isEditing: false
+    }, {
       property: property
     });
   }
   handleEvents() {
     this.on('click', '.edit-btn', () => {
       // show Modal
+      this.setState({
+        isEditing: true
+      });
       console.log('PropertyActions.updateAmount()');
     });
     this.on('click', '.destroy-btn', () => {
@@ -19,12 +24,25 @@ export default class PropertyItem extends Component {
   }
   template() {
     return (
-      `<li>
-        <label>${this.props.property.name}</label>
+      `<li>${this.itemTemplate()}</li>`
+    );
+  }
+  itemTemplate() {
+    let itemTemplate = '';
+    if (this.state.isEditing) {
+      itemTemplate =
+        `<form>
+        <input type="text" value="${this.props.property.name}">
+        <input type="number" value="${this.props.property.amount}">
+        <input type="submit" value="[submit]" class="submit-btn">
+        </form>`;
+    } else {
+      itemTemplate =
+        `<label>${this.props.property.name}</label>
         <label>${this.props.property.amount}</label>
         <div class="edit-btn">[edit]</div>
-        <div class="destroy-btn">[destroy]</div>
-      </li>`
-    );
+        <div class="destroy-btn">[destroy]</div>`;
+    }
+    return itemTemplate;
   }
 }
