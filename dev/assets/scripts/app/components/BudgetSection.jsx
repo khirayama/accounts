8ef841bf0f250659e5b9eb1d574116472aca0b9e
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-07-17 07:29:43 +0900

    コメントの追加

---
+++ dev/assets/scripts/app/components/BudgetSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class BudgetSection extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    // TODO: 日割りのときは分割など計算の追加 - state?
+    // TODO: もう一度全体として、どこまでstateか、propsか検討した方がよいかも
+    let budgetItems = this.props.paymentCategories.map((paymentCategory) => {
+      let total = 0;
+      this.props.payments.filter(function (payment) {
+        if (payment.category === paymentCategory.id) total += +payment.amount;
+      });
+      return (<li>{paymentCategory.name} {paymentCategory.budget} {total}</li>);
+    });
+    return <ul>{budgetItems}</ul>;
+  }
+}
